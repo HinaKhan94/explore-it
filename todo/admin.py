2cdeb4bc28b9f28ed64d648d7b6836ae5af090ed
HinaKhan94 <hinakhan94.hk@gmail.com>
2024-06-28 08:01:27 +0000

    add styles to todo details page

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import Category, Todo, Messages
 
-admin.site.register(Category)
-admin.site.register(Todo)
-admin.site.register(Messages)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('rating', 'price')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'message',
+        'submission_date',
+    )
+
+    ordering = ('submission_date',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Todo, TodoAdmin)
+admin.site.register(Messages, MessagesAdmin)
