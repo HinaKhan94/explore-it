e4b423dedb08abcd18d90eb4e5f29d5101bb6498
HinaKhan94 <hinakhan94.hk@gmail.com>
2024-06-28 07:02:07 +0000

    build: todo detail page template, view and urls

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('todo/<slug:slug>/', views.todo_detail, name='todo_detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Todo
 
 def index(request):
     todos = Todo.objects.all()
     return render(request, 'index.html', {'todos': todos})
+
+def todo_detail(request, slug):
+    todo = get_object_or_404(Todo, slug=slug)
+    related_todos = Todo.objects.filter(category=todo.category).exclude(id=todo.id)[:5]
+    
+    return render(request, 'todo_detail.html', {
+        'todo': todo,
+        'related_todos': related_todos,
+        'range': range(5),
+    })
